ce29037c8eb3cf7832e9f546a4930b7c3e4bcd8f
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-06-14 23:34:50 +0100

    Build function to get computer's results and play a round

---
+++ index.js
@@ -0,0 +1,57 @@
+function computerPlay(){
+    var outcomes = ['rock', 'paper', 'scissors']
+    var x = Math.floor((Math.random()*3))
+    return outcomes[x]
+    //console.log(outcomes[x])
+}
+
+
+var playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
+var outcomeComputer = computerPlay()
+console.log(playRound(outcomeComputer, playerSelection))
+
+function playRound(computer,player){
+    switch(computer){
+        case 'rock':
+           if( player == 'rock'){
+            print("It's a tie!")
+            //return("It's a tie!")
+           } else if (player == 'paper'){
+            print("You won!")
+            //return ("You won!")
+           } else {
+            print("You lost")
+            //return ("You lost")
+           }
+           break;
+        case 'paper':
+            if( player == 'paper'){
+                print("It's a tie!")
+                //return("It's a tie!")
+            } else if (player == 'scissors'){
+                print("You won!")
+                //return ("You won!")
+            } else {
+                print("You lost")
+                //return ("You lost")
+            } 
+            break;
+        case 'scissors':
+            if( player == 'scissors'){
+                //print("It's a tie!")
+                return("It's a tie!")
+            } else if (player == 'rock'){
+                //print("You won!")
+                return ("You won!")
+            } else {
+                //print("You lost")
+                return ("You lost")
+            }   
+            
+
+}
+}
+
+
+
+
