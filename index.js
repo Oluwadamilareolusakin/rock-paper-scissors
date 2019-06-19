cb8d194f875576d841ec8c4863039eb601ebd7a6
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-06-19 20:02:23 +0100

    Restore to original excercise without UI

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-var score = [0,0]
-
-
-
 function computerPlay(){
     var outcomes = ['rock', 'paper', 'scissors']
     var x = Math.floor((Math.random()*3))
@@ -9,102 +5,55 @@ function computerPlay(){
     //console.log(outcomes[x])
 }
 
+document.addEventListener("DOMContentLoaded", () =>{
+    let go = document.querySelector('button');
+    go.addEventListener('click',playRound) 
+})
 
-function playRound(computer,player,score,numberOfGames){
-    
+
+const playRound = () =>{
+    let player = document.getElementById('player-selection').value.toLowerCase();
+    let computer = computerPlay();
     switch(computer){
         case 'rock':
-           if( player == 'paper'){
-            //print("It's a tie!");
-            score[0] = score[0]+1;
-            //print("You won!");
-            return ("You won!",score[0])
-           } else if (player == 'rock'){
-            return("It's a tie!")
-           } else if(player == 'scissors'){
-            //print("You lost");
-            score[1] = score[1]+1;
-            return ("You lost")
+           if( player == 'rock'){
+            console.log("It's a tie!")
+            //return("It's a tie!")
+           } else if (player === 'paper'){
+            console.log("You won!")
+            //return ("You won!")
+           } else {
+            console.log("You lost")
+            //return ("You lost")
            }
-           numberOfGames = numberOfGames - 1
            break;
         case 'paper':
             if( player == 'paper'){
-                //print("It's a tie!")
-                return("It's a tie!")
+                console.log("It's a tie!")
+                //return("It's a tie!")
             } else if (player == 'scissors'){
-                //print("You won!")
-                score[0] = score[0]+1;
-                return ("You won!",score[0])
-            } else if (player == 'rock') {
-                //print("You lost")
-                score[1] = score[1]+1;
-                return ("You lost")
+                console.log("You won!")
+                //return ("You won!")
+            } else {
+                console.log("You lost")
+                //return ("You lost")
             } 
-            numberOfGames = numberOfGames - 1
             break;
         case 'scissors':
             if( player == 'scissors'){
-                //print("It's a tie!")
-                return("It's a tie!")
+                //console.log("It's a tie!")
+                console.log("It's a tie!")
             } else if (player == 'rock'){
-                //print("You won!")
-                score[0] = score[0]+1;
-                return ("You won!",score[0])
-            } else if(player == 'paper') {
-                //print("You lost")
-                score[1] = score[1]+1;
-                return ("You lost")
-                
+                //console.log("You won!")
+                console.log("You won!")
+            } else {
+                //console.log("You lost")
+                console.log("You lost")
             }   
             
-            
+
 }
 }
-function submitPlayerSelection(){
-
-    var playerSelection = document.getElementById("player-selection").value
-    var outcomeComputer = computerPlay();
-    var numberOfGames = document.getElementById('number-of-games').value
-    if( numberOfGames > 0){
-        document.getElementById('number-of-games-container').style = 'display:none'
-    }
-    if (numberOfGames = 0){
-        document.getElementById('number-of-games-container').style = 'display:none'
-        document.getElementById('player-selection-container').style = 'display:none'
-    }
-    
-    var x = document.getElementById("number-of-games")
-    
-    playRound(outcomeComputer,playerSelection,score,numberOfGames)
-    
-    document.getElementById("game-score").innerHTML = 'Human: ' + score[0] + ' - ' + score[1] + ' : Computer'
-    document.getElementById('number-of-games').stepDown(1)
-    console.log(numberOfGames)
-}
-
-/*function game(){
-      var  count;
-      score = [0,0]
-      for(count = 5; count > 0; count--){
-        var playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
-        var outcomeComputer = computerPlay();
-        var res = playRound(playerSelection, outcomeComputer,score);
-        
-        console.log(score);
-        console.log(res)
-      };
-      if (score[0] > score[1]){
-        final = 'Human player won'
-    } else if (score[0] < score[1]){
-        final = 'Computer won'
-    } else {
-        final = "It's a tie"
-    }
-    console.log(final)
-}*/
-
-/*game()*/
 
 
 
