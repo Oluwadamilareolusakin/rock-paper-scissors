954f51f16b16422caed216314e8ffc7fca7b6e95
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-06-24 20:13:28 +0100

    Start building version with button options in the UI

--- index.js
+++ index.js
@@ -6,14 +6,22 @@ function computerPlay(){
 }
 
 document.addEventListener("DOMContentLoaded", () =>{
+    let ccs;
     let go = document.querySelector('button');
     go.addEventListener('click',playRound) 
+    ccs+=playRound.ccs
+    console.log(ccs)
 })
 
 
 const playRound = () =>{
     let player = document.getElementById('player-selection').value.toLowerCase();
     let computer = computerPlay();
+    let ccs;
+    let chs;
+    let compscore=0;
+    let humscore = 0;
+
     switch(computer){
         case 'rock':
            if( player == 'rock'){
@@ -21,10 +29,12 @@ const playRound = () =>{
             //return("It's a tie!")
            } else if (player === 'paper'){
             console.log("You won!")
+            humscore+=1;
             //return ("You won!")
            } else {
             console.log("You lost")
             //return ("You lost")
+            compscore+=1;
            }
            break;
         case 'paper':
@@ -33,10 +43,12 @@ const playRound = () =>{
                 //return("It's a tie!")
             } else if (player == 'scissors'){
                 console.log("You won!")
+                humscore+=1;
                 //return ("You won!")
             } else {
                 console.log("You lost")
                 //return ("You lost")
+                compscore+=1;
             } 
             break;
         case 'scissors':
@@ -46,12 +58,13 @@ const playRound = () =>{
             } else if (player == 'rock'){
                 //console.log("You won!")
                 console.log("You won!")
+                humscore+=1;
             } else {
                 //console.log("You lost")
+                console.log(compscore+=1)
                 console.log("You lost")
             }   
-            
-
+    return chs += humscore, ccs += compscore;
 }
 }
 
